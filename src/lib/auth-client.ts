b658fd3729521ee8cdddf32a561fe1c0ec6bfd74
shadin khan <13307041@iubat.edu>
2026-02-25 22:51:46 +0600

    update

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-     baseURL: typeof window !== "undefined" ? window.location.origin : "",
+     baseURL: "https://nextjs-skill-bridge-backend-project.onrender.com",
+     
   fetchOptions: {
     credentials: "include",
   },
